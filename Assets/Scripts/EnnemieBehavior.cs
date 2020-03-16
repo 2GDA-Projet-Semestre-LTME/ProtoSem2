96d8d336a5ccd5c5bb7f2f10c3bc362a24bea013
Théo Pinto <decratosgames@gmail.com>
2020-03-16 15:18:28 +0100

    Spawner d'NMI et changement des touches

---
+++ Assets/Scripts/EnnemieBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemieBehavior : MonoBehaviour
+{
+    [SerializeField] private float vie;
+
+    private GameObject player;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float deathTimer;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        Chase();
+        if (vie <= 0)
+        {
+            deathTimer -= 1 * Time.deltaTime;
+        }
+
+        if (deathTimer <= 0)
+        {
+                Destroy(gameObject);
+        }
+    }
+
+    private void Chase()
+    {
+        transform.LookAt(player.transform);
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    public void ApplyDammage(float dammage)
+    {
+        vie -= dammage;
+    }
+}
---
+++ Assets/Scripts/EnnemieSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemieSpawner : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> NMI;
+    [SerializeField] private List<GameObject> SpawnedNMI;
+
+    [SerializeField] private float spawnRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 2, spawnRate);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        SpawnedNMI.Add(Instantiate(NMI[0], this.transform.position, this.transform.rotation));
+    }
+}
